396ba327757ccf3aa7e80d67e64c8e8127bc5472
Connor <cmurphy51@student.monroecc.edu>
2024-09-18 22:26:41 +0000

    week 1

---
+++ Week1/calculate_area.m
@@ -0,0 +1,5 @@
+length = 10; % declaring variable for length
+width = 12;  % declaring variable for width
+
+area = length * width; % the area function
+fprintf('The area of the rectangle is %.2f square units.\n', area); % printing the result
---
+++ Week1/week_1commands.m
@@ -0,0 +1,5 @@
+% Connor Murphy
+a = 5
+b = 3
+sum = a + b
+disp 'sum';
