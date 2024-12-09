105ee84e72fe9d8c67001e521e39e6e3265ba6a5
cmurphy51 <cmurphy51@student.monroecc.edu>
2024-12-09 17:59:52 -0500

    Add files via upload

--- Week1/calculate_area.m
+++
@@ -1,5 +0,0 @@
-length = 10; % declaring variable for length
-width = 12;  % declaring variable for width
-
-area = length * width; % the area function
-fprintf('The area of the rectangle is %.2f square units.\n', area); % printing the result
--- Week1/week_1commands.m
+++
@@ -1,5 +0,0 @@
-% Connor Murphy
-a = 5
-b = 3
-sum = a + b
-disp 'sum';
